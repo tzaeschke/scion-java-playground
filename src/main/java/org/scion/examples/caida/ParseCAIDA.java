006282e28bb896c7e74875a0d77eaaed06c31e79
Tilmann Zäschke <tilmann.zaeschke@inf.ethz.ch>
2024-07-05 16:14:37 +0200

    CAIDA parser

--- src/main/java/org/scion/examples/caida/ParseCAIDA.java
+++ src/main/java/org/scion/examples/caida/ParseCAIDA.java
@@ -22,13 +22,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
+
+import jnr.ffi.annotations.In;
 import org.scion.jpan.ScionRuntimeException;
 import org.scion.jpan.internal.HostsFileParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ParseCAIDA {
-  private static final Logger LOG = LoggerFactory.getLogger(HostsFileParser.class);
+  private static final Logger LOG = LoggerFactory.getLogger(ParseCAIDA.class);
   private static final String fileName = "201603.as-rel-geo.txt";
 
   // We use hostName/addressString as key.
@@ -54,21 +56,21 @@ public class ParseCAIDA {
   }
 
   public static class AS {
-    private final int as;
+    private final int asId;
     private String name;
     private final ArrayList<Link> links = new ArrayList<>();
 
-    AS(int as, String name) {
-      this.as = as;
+    AS(int asId, String name) {
+      this.asId = asId;
       this.name = name;
     }
 
-    AS(int as) {
-      this.as = as;
+    AS(int asId) {
+      this.asId = asId;
     }
 
-    public int getAs() {
-      return as;
+    public int getAsId() {
+      return asId;
     }
 
     public String getName() {
@@ -84,9 +86,27 @@ public class ParseCAIDA {
         this.name = name;
       }
     }
+
+    @Override
+    public String toString() {
+      return "as=" + asId + "; name=" + name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof AS) {
+        return ((AS) obj).asId == asId;
+      }
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      return Integer.valueOf(asId).hashCode();
+    }
   }
 
-  public ParseCAIDA() {}
+  private ParseCAIDA() {}
 
   public void init() {
     URL r = getClass().getClassLoader().getResource(fileName);
@@ -98,11 +118,11 @@ public class ParseCAIDA {
     try {
       path = Paths.get(r.toURI());
     } catch (URISyntaxException e) {
-      throw new RuntimeException(e);
+      throw new IllegalArgumentException(e);
     }
 
     try (Stream<String> lines = Files.lines(path)) {
-      lines.forEach((line) -> parseLine(line, path));
+      lines.forEach(line -> parseLine(line, path));
     } catch (IOException e) {
       throw new ScionRuntimeException(e);
     }
@@ -117,8 +137,8 @@ public class ParseCAIDA {
       String[] lineParts = s.split("\\|");
       int asId1 = Integer.parseInt(lineParts[0]);
       int asId2 = Integer.parseInt(lineParts[1]);
-      AS as1 = allAS.computeIfAbsent(asId1, id -> new AS(id));
-      AS as2 = allAS.computeIfAbsent(asId2, id -> new AS(id));
+      AS as1 = allAS.computeIfAbsent(asId1, AS::new);
+      AS as2 = allAS.computeIfAbsent(asId2, AS::new);
       Link link = new Link(asId1, asId2);
       allLinks.add(link);
       as1.addLink(link);
@@ -134,14 +154,31 @@ public class ParseCAIDA {
   public static void main(String[] args) throws URISyntaxException {
     ParseCAIDA pa = new ParseCAIDA();
     pa.init();
+    pa.run();
+  }
 
-    System.out.println("Found links: " + pa.allLinks.size());
-    System.out.println("Found AS: " + pa.allAS.size());
+  private void run() {
+    println("Found links: " + allLinks.size());
+    println("Found AS: " + allAS.size());
+
+    // Find unknown ASes
+    for (Link link : allLinks) {
+      if (!allAS.containsKey(link.getAs2())) {
+        println("Adding: " + link.getAs2());
+        allAS.put(link.getAs2(), new AS(link.getAs2(), "UNKNOWN-" + link.getAs2()));
+      }
+      if (!allAS.containsKey(link.getAs1())) {
+        println("Adding: " + link.getAs1());
+        allAS.put(link.getAs1(), new AS(link.getAs1(), "UNKNOWN-" + link.getAs1()));
+      }
+    }
+
+    // build histogram
     int maxLinks =
-        pa.allAS.values().stream().map(as -> as.links.size()).max((o1, o2) -> o1 - o2).get();
+        allAS.values().stream().map(as -> as.links.size()).max((o1, o2) -> o1 - o2).get();
     int[] histo = new int[maxLinks];
-    AS maxAS = pa.allAS.values().iterator().next();
-    for (AS as : pa.allAS.values()) {
+    AS maxAS = allAS.values().iterator().next();
+    for (AS as : allAS.values()) {
       int links = as.links.size();
       histo[links - 1]++;
       if (as.links.size() > maxAS.links.size()) {
@@ -149,14 +186,26 @@ public class ParseCAIDA {
       }
     }
 
-    System.out.println("Histo: " + Arrays.toString(histo));
-    System.out.println(
-        "Max: " + maxAS.links.size() + " links in " + maxAS.as + " - " + maxAS.getName());
+    // print histogram
+    println("Histo: " + Arrays.toString(histo));
+    println("Max: " + maxAS.links.size() + " links in " + maxAS.getAsId() + "/" + maxAS.getName());
 
+    // print list links/AS
     for (int i = 0; i < histo.length; i++) {
       if (histo[i] > 0) {
-        System.out.println("links/ASes = " + (i + 1) + " / " + histo[i]);
+        println("links/ASes = " + (i + 1) + " / " + histo[i]);
       }
     }
+
+    println("NOTE: These are inferred RELATIONSHIPS, not LINKS.");
+    println("Each relationship may consists of any number of links!");
+  }
+
+  private static void print(String str) {
+    System.out.print(str);
+  }
+
+  private static void println(String str) {
+    System.out.println(str);
   }
 }
